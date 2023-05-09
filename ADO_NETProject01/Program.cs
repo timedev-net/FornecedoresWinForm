c3d7c44c28e6eca291d90db24e57e00c9a40c562
Daniel Frota <drfrota.adv@gmail.com>
2023-05-08 23:59:53 -0400

    first commit

---
+++ ADO_NETProject01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADO_NETProject01
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
